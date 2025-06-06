99f4caabd72b9b037b0f1a16c6ad530152376457
Azrad M <nurulazrad@gmail.com>
2025-06-06 13:02:53 +0800

    feat: improve health monitor script

--- scripts/heartbeat-monitor.sh
+++ scripts/heartbeat-monitor.sh
@@ -2,8 +2,9 @@
 
 # Usage: ./heartbeat-monitor.sh <URL>
 URL="$1"
-DURATION=30
+DURATION=120
 INTERVAL=1
+ENDPOINTS=("/health" "/ready" "/startup")
 
 # Validate input
 if [ -z "$URL" ]; then
@@ -11,22 +12,45 @@ if [ -z "$URL" ]; then
     exit 1
 fi
 
-echo "Starting HTTP request loop for $DURATION seconds..."
-echo "Target URL: $URL"
-echo "-------------------------------------------"
+echo "Starting health check loop for $DURATION seconds..."
+echo "--------------------------------------------------------------------------"
+printf "| %-15s | %-10s | %-50s |\n" "Path URL" "Response" "Body"
+echo "--------------------------------------------------------------------------"
+
+CURL_CMD=$(which curl)
+CAT_CMD=$(which cat)
+SLEEP_CMD=$(which sleep)
 
 for ((i=1; i<=$DURATION; i++)); do
-    RESPONSE=$(curl -s -o response.txt -w "%{http_code}" "$URL")
-    BODY=$(cat response.txt)
-    
-    echo "[Request #$i] URL: $URL"
-    echo "HTTP Response: $RESPONSE"
-    echo "Body Response:"
-    echo "$BODY"
-    echo "-------------------------------------------"
-    
-    sleep $INTERVAL
+    for PATH in "${ENDPOINTS[@]}"; do
+        RESPONSE=$($CURL_CMD -s -o response.txt -w "%{http_code}" "$URL$PATH")
+        BODY=$($CAT_CMD response.txt)
+
+        printf "| %-15s | %-10s | %-50s |\n" "$PATH" "$RESPONSE" "$BODY"
+    done
+    echo "--------------------------------------------------------------------------"
+
+    $SLEEP_CMD $INTERVAL
 done
+
+echo "--------------------------------------------------------------------------"
+
+# echo "Starting HTTP request loop for $DURATION seconds..."
+# echo "Target URL: $URL"
+# echo "-------------------------------------------"
+# 
+# for ((i=1; i<=$DURATION; i++)); do
+#     RESPONSE=$(curl -s -o response.txt -w "%{http_code}" "$URL")
+#     BODY=$(cat response.txt)
+#     
+#     echo "[Request #$i] URL: $URL"
+#     echo "HTTP Response: $RESPONSE"
+#     echo "Body Response:"
+#     echo "$BODY"
+#     echo "-------------------------------------------"
+#     
+#     sleep $INTERVAL
+# done
 
 echo "Loop completed."
 
